a94a9c1eaca2e38855d90faafdee6587fb619006
goropikari <goropikari56@gmail.com>
2025-03-23 04:20:01 +0900

    add clang

--- docker/langs/build.sh
+++ docker/langs/build.sh
@@ -5,6 +5,7 @@ set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 docker build -t library-checker-images-gcc -f $SCRIPT_DIR/Dockerfile.GCC $SCRIPT_DIR
+docker build -t library-checker-images-clang -f $SCRIPT_DIR/Dockerfile.CLANG $SCRIPT_DIR
 docker build -t library-checker-images-python3 -f $SCRIPT_DIR/Dockerfile.PYTHON3 $SCRIPT_DIR
 docker build -t library-checker-images-pypy -f $SCRIPT_DIR/Dockerfile.PYPY $SCRIPT_DIR
 docker build -t library-checker-images-golang -f $SCRIPT_DIR/Dockerfile.GOLANG $SCRIPT_DIR
